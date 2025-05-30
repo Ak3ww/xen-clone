34cab8254833684cea528d8fa6c7eceba4ab71d9
ROLF <rakaharyadi12@gmail.com>
2025-05-30 11:34:03 +0700

    Update abi.js

--- abi.js
+++ abi.js
@@ -1,1 +1,60 @@
-
+export const XEN_ABI = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [{"internalType": "uint256","name": "term","type": "uint256"}],
+    "name": "claimRank",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "claimMintReward",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "globalRank",
+    "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [{"internalType": "address","name": "","type": "address"}],
+    "name": "balanceOf",
+    "outputs": [{"internalType": "uint256","name": "","type": "uint256"}],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "claimMintReward",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "userMints",
+    "outputs": [
+      {"internalType": "uint256","name": "term","type": "uint256"},
+      {"internalType": "uint256","name": "maturityTs","type": "uint256"},
+      {"internalType": "uint256","name": "rank","type": "uint256"}
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
